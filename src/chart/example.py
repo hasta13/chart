974d5431aae167a889f88c4149b6624178711314
Hal Allred <halleydallred@gmail.com>
2021-07-04 00:31:57 -0600

    Re-structured the entire project based on tutorial from packaging.python.org. Now it works!!

---
+++ src/chart/example.py
@@ -0,0 +1,17 @@
+# This library will be used for making charts easier
+# for Network Performance Engineers
+#
+# This package was creating following this tutorial:
+# https://packaging.python.org/tutorials/packaging-projects/
+#
+# To update the package on PyPI:
+# 1. Update version number in setup.cfg
+# 2. In terminal, build the new version: "python3 -m build"
+# 6. Upload to PyPI: "twine upload dist/*"
+# 7. Enter username and password for PyPI. Done!
+#
+# To install the package. Do this from the terminal:
+# python3 -m pip install "hallred-chart"
+
+def test():
+    print('this is a placeholder')
